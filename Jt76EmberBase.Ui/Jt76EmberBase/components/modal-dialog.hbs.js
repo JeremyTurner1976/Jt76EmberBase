2c18587dcad2408425e0a76c3336beb0d7b83986
JeremyTurner1976 <jeremyturner1976@yahoo.com>
2015-03-28 18:31:53 -0700

    smaller close button for taller layouts

--- Jt76EmberBase.Ui/Jt76EmberBase/components/modal-dialog.hbs.js
+++ Jt76EmberBase.Ui/Jt76EmberBase/components/modal-dialog.hbs.js
@@ -4,7 +4,7 @@
     + escapeExpression(((helpers.action || (depth0 && depth0.action) || helperMissing).call(depth0, "close", {"name":"action","hash":{},"data":data})))
     + ">\r\n    <div class=\"pull-right text-success\" "
     + escapeExpression(((helpers.action || (depth0 && depth0.action) || helperMissing).call(depth0, "close", {"name":"action","hash":{},"data":data})))
-    + "><i class=\"fa fa-check-circle\" style=\"font-size:50px;padding:8px;\"></i></div>\r\n</div>\r\n<div class=\"jt76-modal-dialog\">\r\n    "
+    + "><i class=\"fa fa-check-circle\" style=\"font-size:44px;padding:6px;\"></i></div>\r\n</div>\r\n<div class=\"jt76-modal-dialog\">\r\n    "
     + escapeExpression(((helper = (helper = helpers['yield'] || (depth0 != null ? depth0['yield'] : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"yield","hash":{},"data":data}) : helper)))
     + "\r\n</div>";
 },"useData":true});
